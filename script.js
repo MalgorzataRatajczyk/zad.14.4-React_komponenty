a8df03c16eeb71fa683406b18e64c30cfbde3d7e
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-10-06 20:07:43 +0200

    Final correction

--- script.js
+++ script.js
@@ -64,73 +64,81 @@ var Movie = React.createClass({
 
             render: function() {
                 return (
-                    React.createElement(Movie, {key: movies.src}, this.props.movies.src) 
+                    React.createElement(Movie, {key: movies.src}, this.props.movies.src)
                 )
             }
         });
     }
 });
 
+// tablica z danymi filmów
 var movies = [
     { 
         id: 1,
         title: 'Dawno temu w Ameryce',
-        desc: 'Dramat, gangsterski',
-        director: 'Sergio Leone',
-        year: 1984,
-        src: '/image/Dawno_temu.jpg'
+        desc: 'Gatunek: Dramat, gangsterski',
+        director: 'Reżyser: Sergio Leone',
+        year: 'Data premiery: ' + 1984,
+        src: './image/Dawno_temu.jpg'
     },
     { 
         id: 2,
         title: 'Chłopcy z Ferajny',
-        desc: 'Dramat, gangsterski',
-        director: 'Martin Scorsese',
-        year: 1990,
-        src: '/image/Chłopcy_z_ferajny.jpg'
+        desc: 'Gatunek: Dramat, gangsterski',
+        director: 'Reżyser: Martin Scorsese',
+        year: 'Data premiery: ' + 1990,
+        src: './image/Chłopcy_z_ferajny.jpg'
     },
     { 
         id: 3,
         title: 'Gorączka',
-        desc: 'Sensacyjny',
-        director: 'Michael Mann',
-        year: 1995,
-       src: '/image/Gorączka.jpg'
+        desc: 'Gatunek: Sensacyjny',
+        director: 'Reżyser: Michael Mann',
+        year: 'Data premiery: ' + 1995,
+        src: './image/Gorączka.jpg'
     },
     { 
         id: 4,
         title: 'Ojciec Chrzestny',
-        desc: 'Dramat, gangsterski',
-        director: 'Francis Ford Coppola',
-        year: 1972,
-        src: '/image/Ojciec_Chrzestny.jpg'
+        desc: 'Gatunek: Dramat, gangsterski',
+        director: 'Reżyser: Francis Ford Coppola',
+        year: 'Data premiery: ' + 1972,
+        src: './image/Ojciec_Chrzestny.jpg'
     },
     { 
         id: 5,
         title: 'Ojciec Chrzestny II',
-        desc: 'Dramat, gangsterski',
-        director: 'Francis Ford Coppola',
-        year: 1974,
-        src: '/image/Ojciec_..._II.jpg'
+        desc: 'Gatunek: Dramat, gangsterski',
+        director: 'Reżyser: Francis Ford Coppola',
+        year: 'Data premiery: ' + 1974,
+        src: './image/Ojciec_..._II.jpg'
     },
     { 
         id: 6,
         title: 'Człowiek z blizną',
-        desc: 'Dramat, gangsterski',
-        director: 'Francis Ford Coppola',
-        year: 1983,
-        src: '/image/Człowiek_z_blizną.jpg'
+        desc: 'Gatunek: Dramat, gangsterski',
+        director: 'Reżyser: Brian De Palma',
+        year: 'Data premiery: ' + 1983,
+        src: './image/Człowiek_z_blizną.jpg'
     }
 ];
 
- var moviesElements = movies.map(function(movie) {
+
+/* zmapowanie listy na odpowiadające im Reactowe elementy za pomocą metody .map(),
+która przyjmuje jako parametr funkcję, przez którą przechodzi każdy z elementów tablicy (czyli każdy film),
+który następnie jest mapowany do postaci ReactElementu korzystającego z informacji o filmie (movie.title, movie.desc itd. */ 
+
+var moviesElements = movies.map(function(movie) {
     return React.createElement('li', {key: movie.id},
         React.createElement('h2', {}, movie.title),
         React.createElement('p', {}, movie.desc),
         React.createElement('p', {}, movie.director),
         React.createElement('p', {}, movie.year),
-        React.createElement('img', {}, movies.img),
+        React.createElement('img', {src: movie.src}),
     );
  });
+
+// umieszczenie elementów w liście
 
  var element =
     React.createElement('div', {},
